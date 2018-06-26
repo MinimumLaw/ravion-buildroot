187bd7ffee05abc1a6ab018d8d6c730e7d23049b
Alex A. Mihaylov <minimumlaw@rambler.ru>
2018-06-26 21:11:48 +0300

    Fix uEnv.txt placement

--- board/variscite/mx6ul_var_dart/post-image.sh
+++ board/variscite/mx6ul_var_dart/post-image.sh
@@ -4,7 +4,7 @@ GENIMAGE_CFG="$(dirname $0)/genimage.cfg"
 GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
 
 # copy the uEnv.txt to the output/images directory
-cp board/toradex/apalis-imx6/uEnv.txt $BINARIES_DIR/uEnv.txt
+cp board/variscite/mx6ul_var_dart/uEnv.txt $BINARIES_DIR/uEnv.txt
 
 rm -rf "${GENIMAGE_TMP}"
 
