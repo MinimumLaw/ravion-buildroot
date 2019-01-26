b01ea3e17becef3431c3fa1d55c02095875fdb44
Alex A. Mihaylov <minimumlaw@rambler.ru>
2019-01-26 11:43:46 +0300

    Работа над ошибками: запускаем Raspberri с Buildroot
      На базе выяснилось, что дерево устройств не от того модуля. У нас
    используется RaspberryPi Zero W, а я с чего-то решил что Raspberry3
    
      На базе была подменена DTB, но этого мало. Помимо DTB надо менять
    конфиг ядра и поддерживаемую  архитектуру для  всего прикладного ПО.
      В таком варианте мы начинаем работать.

--- build-raspbery.sh
+++ build-raspbery.sh
@@ -5,6 +5,6 @@ export DEFARGS="-j`grep -i processor /proc/cpuinfo | wc -l`"
 if [ -f .config ]; then
 	make ${DEFARGS} $*
 else
-	make stp_raspberrypi3_defconfig
+	make stp_raspberrypi0w_defconfig
 	make ${DEFARGS}
 fi
