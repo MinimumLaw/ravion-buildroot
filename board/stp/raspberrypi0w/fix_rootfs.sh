5f53bc7ee3e2fa559b4561f9c2706a1bef141f0e
Alex A. Mihaylov <minimumlaw@rambler.ru>
2019-02-26 20:54:29 +0300

    На базе:
    Проверено, на A1 надо отключать 4G
    
    ToDo:
    - права доступа на каталог /var/www/html
    - права доступа ПОСЛЕ записи файлов через SCP
    - обновления базовой системы, ядра, и информационных файлов
    - серийныый номер на STM и Raspbery
    - modem
    - сеть без интернета...
    - прочее и остальное =)

---
+++ board/stp/raspberrypi0w/fix_rootfs.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@ ROOTFS FIXUP STARTED @@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+echo ToDo: add required code here
+
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@  ROOTFS FIXUP ENDED  @@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@
