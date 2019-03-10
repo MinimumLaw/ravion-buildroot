a0f0159841db73dc6e5dbebfdb5379a8b7fa8856
Alex A. Mihaylov <minimumlaw@rambler.ru>
2019-03-10 19:34:25 +0300

    RaspberryPI
    [+] - данные теперь обновляются (с контролем), и могут быть запакованы
          от имени любого пользователя
    [=] - stuff переименован в staff (grammatic fix)
    [!] - www-data (apache) снова может нормально запускать call.php
    [*] - user-table теперь не нужна, из defconfig убрал BIND
    [*] - initscripts & users перетащены в stp-tools (переносимость на var. som)
    
    KnownBugs:
    [?] - www-data никак не хочет работать с sftp. Хоть убейся. Не понимаю...

--- package/stp-tools/stp-tools.mk
+++ package/stp-tools/stp-tools.mk
@@ -3,7 +3,7 @@
 ##############################################################################
 
 STP_TOOLS_VERSION = origin/test-for-new-firmware
-STP_TOOLS_SITE = git@bitbucket.org:DenisKalou/raspberry_uart.git
+STP_TOOLS_SITE = git@bitbucket.org:DenisKalou/speakingcity_raspbery.git
 STP_TOOLS_SITE_METHOD = git
 STP_TOOLS_LICENSE = PROPRIETARY
 # STP-TOOLS_DEPENDENCIES = ???
@@ -16,6 +16,19 @@ define STP_TOOLS_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/call $(TARGET_DIR)/usr/local/bin/call
 	$(INSTALL) -D -m 0755 $(@D)/gps $(TARGET_DIR)/usr/local/bin/gps
 	$(INSTALL) -D -m 0755 $(@D)/sc_uart $(TARGET_DIR)/usr/local/bin/sc_uart
+endef
+
+define STP_TOOLS_USERS
+staff 50 staff -1 =staff /overlay/update /bin/sh wheel,daemon,video,audio,disk,tty SpeakingSity update
+endef
+
+define STP_TOOLS_INSTALL_INIT_SYSV
+	$(INSTALL) -D -m 0755 package/stp-tools/S01wireless-comm \
+		$(TARGET_DIR)/etc/init.d/S01wireless-comm
+	$(INSTALL) -D -m 0755 package/stp-tools/S98update-data \
+		$(TARGET_DIR)/etc/init.d/S98update-data
+	$(INSTALL) -D -m 0755 package/stp-tools/S99stp-tools \
+		$(TARGET_DIR)/etc/init.d/S99stp-tools
 endef
 
 $(eval $(generic-package))
