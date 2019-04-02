6d76c8e57587b71a6bf44b4bde030ae8bafb9fb8
Alex A. Mihaylov <minimumlaw@rambler.ru>
2019-04-02 18:14:02 +0300

    Add sc_ble_scan (forgotten for commit)

--- package/stp-tools/stp-tools.mk
+++ package/stp-tools/stp-tools.mk
@@ -16,6 +16,7 @@ define STP_TOOLS_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/source/call $(TARGET_DIR)/usr/local/bin/call
 	$(INSTALL) -D -m 0755 $(@D)/source/gps $(TARGET_DIR)/usr/local/bin/gps
 	$(INSTALL) -D -m 0755 $(@D)/source/sc_uart $(TARGET_DIR)/usr/local/bin/sc_uart
+	$(INSTALL) -D -m 0755 $(@D)/source/sc_ble_scan $(TARGET_DIR)/usr/local/bin/sc_ble_scan
 endef
 
 define STP_TOOLS_USERS
